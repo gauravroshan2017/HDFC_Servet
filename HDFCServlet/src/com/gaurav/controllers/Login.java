de7fa340711d59cfe4e23002b04f57a6316afc2b
Awasthi <Awasthi@Awasthi>
2017-06-20 12:09:01 +0530

    initial message set up

---
+++ HDFCServlet/src/com/gaurav/controllers/Login.java
@@ -0,0 +1,51 @@
+package com.gaurav.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gaurav.daoImpl.Authenticator;
+import com.gaurav.model.User;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		RequestDispatcher rd = null;
+ 
+		Authenticator authenticator = new Authenticator();
+		String result = authenticator.authenticate(username, password);
+		if (result.equals("success")) {
+			rd = request.getRequestDispatcher("/success.jsp");
+			User user = new User(username, password);
+			request.setAttribute("user", user);
+		} else {
+			rd = request.getRequestDispatcher("/error.jsp");
+		}
+		rd.forward(request, response);
+	}
+	}
+
+
